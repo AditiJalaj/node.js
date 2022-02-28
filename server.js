14c4eb72965740dd5063dd6cec00e750030bf80f
Aditi Jalaj <52830622+AditiJalaj@users.noreply.github.com>
2022-02-28 22:09:10 +0530

    send response

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const http = require('http');
 //creating a server instance which has callback as arg that runs everytime a request comes in
 const server = http.createServer((req, res) => {
   console.log('request made');
+  console.log(req.url, req.method);
+
+  //to send response to the browser - following 3 steps
+  //1 setting the Response header
+  res.setHeader('Content-Type', 'text/html');
+
+  //2 write to the browser
+  res.write('<h1>Hello Aditi <h2>');
+
+  //3 ending the content
+  res.end();
 });
 
 //3rd arg cb fires when we are listening for requests
